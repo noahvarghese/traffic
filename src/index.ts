7ec1a1a20a21f7ffb8980f668839453e5b8f2797
noahvarghese <varghese.noah@gmail.com>
2020-11-08 19:12:10 -0500

    retrieve oauth token

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// This is a placeholder
+import express from "express";
+import axios from "axios";
+import { generateToken } from "./lib/Login";
+
+(async () => {
+    const app = express();
+    const port = 4000;
+
+    const token = await generateToken();
+
+    console.log(token);
+
+    app.listen(port, () => {
+        console.log(`Server started on port: ${port}.`);
+    });
+})();
