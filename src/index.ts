e1fb204adaf3ca2f4ceb1d6e155643a6587ed43c
noahvarghese <varghese.noah@gmail.com>
2020-11-13 23:30:45 -0500

    got town road data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
 // import axios from "axios";
 import * as dotenv from "dotenv";
+import { loadCsvFile } from "./lib/readCSV";
+
 dotenv.config({ path: __dirname + "/.env" });
 
 (async () => {
+    const mapData = (await loadCsvFile(
+        __dirname + "/data/Road_Network.csv"
+    )) as any[];
+    console.log(Object.keys(mapData[0]));
+    console.log(mapData[0].ROUTE, mapData[0].SHAPESTLength);
     const app = express();
     const port = 4000;
+
+    app.get("/", async (req, res) => {
+        res.send();
+    });
 
     app.listen(port, () => {
         console.log(`Server started on port: ${port}.`);
