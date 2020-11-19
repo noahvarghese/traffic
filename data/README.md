47f921a1f07887b179c472ab9286b968cdcd7a57
noahvarghese <varghese.noah@gmail.com>
2020-11-18 22:50:54 -0500

    got a python file working that does everything ive been trying to do over the past week, but in 6 lines of code

--- data/README.md
+++ data/README.md
@@ -4,7 +4,9 @@
 
 Source
 
--   https://www.openstreetmap.org/api/0.6/relation/2407500/full
+-   Main page: https://www.openstreetmap.org/relation/2407500#map=12/43.4466/-79.7158
+-   file: https://www.openstreetmap.org/api/0.6/relation/2407500/full
+-   saved as ./data/raw/road/town/oakville.osm
 -   osm data as a series of nodes, currently investigating drawing it in python
 -   Python currently looks like a contender for manipulating and visualizing the data
     -   https://towardsdatascience.com/find-and-plot-your-optimal-path-using-plotly-and-networkx-in-python-17e75387b873
--- python/main.py
+++ python/main.py
@@ -1,9 +1,24 @@
 #!/opt/anaconda/bin/python3.8
 
-import osmnx
+import osmnx as ox
 import networkx as nx
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import numpy as np
+import geoplot
 
-province = ox.gdf_from_place('Ontario, CA')
-ox.plot_shape(ox.project_gdf(province))
+# Cache data pulled
+ox.config(use_cache=True, log_console=True)
+
+# Get city boundaries
+city = ox.geocoder.geocode_to_gdf("Oakville, Ontario, CA", which_result=2)
+
+# Project the data onto a Geo Data Frame
+ax = ox.project_gdf(city).plot(fc='none', ec='red')
+
+roads = ox.graph.graph_from_place("Oakville, Ontario, CA", network_type="drive")
+# bx = ox.project_graph(roads).plot_graph()
+ox.plot_graph(roads)
+
+# Show the graph
+plt.show()
