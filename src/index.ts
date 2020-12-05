caf329f2801844441ff226f7d46de47ab5da193a
noahvarghese <varghese.noah@gmail.com>
2020-12-05 18:44:58 -0500

    Got djikstra working

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import Node from "./lib/Classes/Node";
 import Edge from "./lib/Classes/Edge";
 import Car from "./lib/Classes/Car";
 import Djikstra, { orderPath, shortestPath } from "./lib/Functions/Djikstra";
-import { LoadNodes, LoadEdges, random, LinkNodesAndEdges, InitTrafficLights } from "./lib/Functions/Functions";
-
+import {
+    LoadNodes,
+    LoadEdges,
+    random,
+    LinkNodesAndEdges,
+    InitTrafficLights,
+} from "./lib/Functions/Functions";
 
 /************************************
  *  My Traffic Simulation
@@ -14,12 +19,8 @@ import { LoadNodes, LoadEdges, random, LinkNodesAndEdges, InitTrafficLights } fr
  *      000753196
  *************************************/
 
-
-
-
 // use base djikstra, or a star for pathfinding
 const useAStar = true;
-
 
 // Load graph as a set of nodes/vertices and edges
 
@@ -31,7 +32,6 @@ const Intersections: Node[] = LoadNodes("./src/data/Nodes.json");
 // We pass the nodes in so that a lookup can be done and a reference created
 const Roads: Edge[] = LoadEdges("./src/data/Edges.json", Intersections);
 
-
 // set references, so we always have access to all objects, no matter the scope
 // FIXED: need to figure out a way to tie the edges to the correct orientation
 // this also initializes the traffic lights
@@ -39,32 +39,7 @@ const Roads: Edge[] = LoadEdges("./src/data/Edges.json", Intersections);
 LinkNodesAndEdges(Intersections, Roads);
 InitTrafficLights(Intersections);
 
-
-
 // need to init traffic lights
-
-
-
-// create cars
-// const numberOfVehicles: number = 1;
-// const cars: Car[] = [];
-
-// console.log("Started");
-
-// for ( let i = 0; i < numberOfVehicles; i++ ) {
-//     const startingNode: Node = Intersections[ random(Intersections.length) ];
-//     const endingNode: Node = Intersections[ random(Intersections.length) ];
-
-//     const car = new Car(
-//        startingNode,
-//        endingNode,
-//        Djikstra(startingNode, endingNode, Intersections, Roads, useAStar)
-//     );
-
-//     cars.push(car);
-// }
-
-
 
 // generate starting and ending points for the cars
 
@@ -84,25 +59,37 @@ InitTrafficLights(Intersections);
     });
 
     app.post("/renderGraph", (_, res) => {
-
         // create cars
         const numberOfVehicles: number = 1;
         const cars: Car[] = [];
 
-        for ( let i = 0; i < numberOfVehicles; i++ ) {
-            const startingNode: Node = Intersections[ random(Intersections.length) ];
-            const endingNode: Node = Intersections[ random(Intersections.length) ];
+        for (let i = 0; i < numberOfVehicles; i++) {
+            const startingNode: Node =
+                Intersections[random(Intersections.length)];
+            const endingNode: Node =
+                Intersections[random(Intersections.length)];
 
             const car = new Car(
                 startingNode,
                 endingNode,
-                Djikstra(startingNode, endingNode, Intersections, Roads, useAStar)
+                Djikstra(
+                    startingNode,
+                    endingNode,
+                    Intersections,
+                    Roads,
+                    useAStar
+                )
             );
 
             cars.push(car);
         }
 
-        res.send({ Nodes: Intersections, Edges: Roads, Cars: cars, path: shortestPath(cars[0])});
+        res.send({
+            Nodes: Intersections,
+            Edges: Roads,
+            Cars: cars,
+            path: shortestPath(cars[0]),
+        });
     });
 
     const port = 3000;
--- src/lib/Functions/Djikstra.ts
+++ src/lib/Functions/Djikstra.ts
@@ -1,47 +1,62 @@
-import { exit } from "process";
 import Car from "../Classes/Car";
 import Edge from "../Classes/Edge";
 import Node from "../Classes/Node";
 import { EuclidianDistance } from "./TrafficLightSystem";
 
-const aStarMod = ( edge: Edge, targetNode: Node, Nodes: Node[] ) => {
+const aStarMod = (edge: Edge, targetNode: Node, Nodes: Node[]) => {
     const density = edge.queue.density();
     const distance = edge.distance;
 
     const flowRate = density * distance;
 
-    const distanceFromDestination = EuclidianDistance(Node.FindNodeByIntersection(edge.endingNodeIntersection, Nodes)!, targetNode);
+    const distanceFromDestination = EuclidianDistance(
+        Node.FindNodeByIntersection(edge.endingNodeIntersection, Nodes)!,
+        targetNode
+    );
 
     return distanceFromDestination + flowRate;
 };
 
-const minOf = (object: any) => {
+const minOf = (queue: (Node | undefined)[], dist: any) => {
+    const keys = Object.keys(dist);
     let min = { key: "", value: Infinity };
 
-    Object.keys(object).forEach((key) => {
-        if ( object[key] < min.value ) {
-            min = { key, value: object[key]} ;
+    // first get the distances of all nodes that have not been checked
+    // then get the min
+    const filtered = queue
+        .filter((node) => (node ? keys.includes(node?.intersection) : false))
+        .map((node) => {
+            if (node) {
+                if (keys.includes(node?.intersection)) {
+                    return {
+                        key: node.intersection,
+                        value: dist[node.intersection],
+                    };
+                }
+            }
+        });
+
+    Object.keys(filtered).forEach((key: string) => {
+        if (filtered[key].value < min.value) {
+            min = { key: filtered[key].key, value: filtered[key].value };
         }
-    })
+    });
 
     return min;
-}
+};
 
 export default (
-    startingNode: Node, 
-    endNode: Node, 
-    nodes: Node[], 
+    startingNode: Node,
+    endNode: Node,
+    nodes: Node[],
     edges: Edge[],
     useAStar: boolean = true
 ): {} => {
-
-    let queue: (Node | undefined)[] = [];
-    const sequence = [];
+    const queue: (Node | undefined)[] = [];
     const dist: any = {};
     const prev: any = {};
-    let current = startingNode;
 
-    for ( let node of nodes ) {
+    for (const node of nodes) {
         dist[node.intersection] = Infinity;
         prev[node.intersection] = undefined;
         queue.push(node);
@@ -49,53 +64,63 @@ export default (
 
     dist[startingNode.intersection] = 0;
 
-    let u:Node | undefined = new Node("", { latitude: 0, longitude: 0});
+    let u: Node | undefined = new Node("", { latitude: 0, longitude: 0 });
 
-    while ( queue.filter((item) => item !== undefined).length > 0 && u !== undefined ) {
+    while (
+        queue.filter((item) => item !== undefined).length > 0 &&
+        u !== undefined
+    ) {
         // get distance key of min distance value and node associated with it
         // then remove that node from queue
-        const min = minOf(dist);
+        // minof distances of obejcts left in queue
+        // filter distance to only objects in queue
+        // get min of them
+        const min = minOf(queue, dist);
 
         let qIndex = Infinity;
         u = queue.find((item, index) => {
             qIndex = index;
-            // return item!.intersection === min.key
-            if ( item ) {
-                return item.intersection === min.key;
-            }
+            return item?.intersection === min.key;
+            // if (item) {
+            //     return item.intersection === min.key;
+            // }
 
-            return false;
+            // return false;
         });
 
         queue[qIndex] = undefined;
 
-        const neighbors = edges.filter((edge) => edge.startingNodeIntersection === u?.intersection)
-                                .map((edge) => (
-                                    {
-                                        edge, 
-                                        node: 
-                                            nodes.find((node) => (edge.endingNodeIntersection === node.intersection))
-                                    }
-                                ));
+        const neighbors = edges
+            .filter((edge) => edge.startingNodeIntersection === u?.intersection)
+            .map((edge) => ({
+                edge,
+                node: nodes.find(
+                    (node) => edge.endingNodeIntersection === node.intersection
+                ),
+            }));
 
-        for ( let neighbor of neighbors ) {
+        for (const neighbor of neighbors) {
             let length;
 
-
-            if ( useAStar ) {
+            if (useAStar) {
                 length = aStarMod(neighbor.edge, endNode, nodes);
             } else {
                 length = neighbor.edge.distance;
             }
 
             const alt = dist[u?.intersection!] + length;
-            const idx = Object.keys(dist).find((key) => key === neighbor.node?.intersection)!
-        
-            if ( alt < dist[idx]  ) {
+            const idx = Object.keys(dist).find(
+                (key) => key === neighbor.node?.intersection
+            )!;
+
+            if (alt < dist[idx]) {
                 dist[idx] = alt;
                 prev[idx] = u?.intersection;
             }
         }
+        // console.log(queue.map((node) => node?.intersection));
+        // console.log(u);
+        // console.log(min, dist, u);
     }
 
     return prev;
@@ -105,10 +130,8 @@ export default (
 
     // let vertexSet: Node[] = [];
 
-
     // const distance: number[] = [];
     // const previous: (Node | undefined)[] = [];
-    
 
     // // init arrays for tracking
     // for ( let i = 0; i < nodes.length; i++ ) {
@@ -151,7 +174,7 @@ export default (
     //             previous[ nodes.indexOf(neighbor!) ] = u;
     //         }
     //     }
-        
+
     //     // get closest node
     //     const index = distance.indexOf(Math.min(...distance));
     //     u = vertexSet[ index ]
@@ -170,13 +193,13 @@ export const orderPath = (car: Car, nodes: Node[]) => {
     // console.log(previous);
 
     const path: string[] = [];
-    const nodeToNode: any = {}
+    const nodeToNode: any = {};
 
     // assemble link between source and destination nodes
     // key = destination
     // value = previous
     previous.forEach((node: Node | undefined, index: number) => {
-        if ( node ) {
+        if (node) {
             const key = nodes[index].intersection;
             const value = node.intersection;
             nodeToNode[key] = value;
@@ -189,8 +212,8 @@ export const orderPath = (car: Car, nodes: Node[]) => {
 
     path.push(destinationNode);
 
-    while ( previousNode ) {
-        console.log(`Destination: ${destinationNode}`)
+    while (previousNode) {
+        console.log(`Destination: ${destinationNode}`);
         destinationNode = previousNode;
         previousNode = nodeToNode[destinationNode];
         path.push(destinationNode);
@@ -199,23 +222,22 @@ export const orderPath = (car: Car, nodes: Node[]) => {
     // path.push(start);
     // return reversed so the start is the first index
     return path.reverse();
-}
+};
 
 export const shortestPath = (car: Car) => {
     // let s represent a sequence of intersections
-    let s = [];
-    let path = car.path;
-    console.log(path)
-
+    const s = [];
+    const path = car.path;
+    // console.log(car.path);
     let u = car.destinationNode.intersection;
 
-    if ( path[u] || u === car.startingNode.intersection ) {
-        while ( u ) {
+    if (path[u] || u === car.startingNode.intersection) {
+        while (u) {
             s.push(u);
             u = path[u];
         }
     }
 
-    console.log(s);
-    return s;
-}
+    // console.log(s);
+    return s.reverse();
+};
